29ded605b4bf108f637fa9644453edfada58a607
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-09-08 11:06:00 +0300

    add controller to  get all posts

--- backend/src/controllers/postsController.js
+++ backend/src/controllers/postsController.js
@@ -1,1 +1,16 @@
 // handlers for requests on '/posts' endpoints
+
+export async function getAllPosts(req, res) {
+
+    const posts = await getAllPosts();
+
+    if (!posts) {
+        return res.status(500).json({ error: "Internal server error" });
+    }
+
+    if (!posts.length) {
+        return res.status(404).json({ error: "No posts yet" });
+    }
+
+    return res.json({ posts: posts });
+}
--- backend/src/dal/postsDAL.js
+++ backend/src/dal/postsDAL.js
@@ -3,13 +3,17 @@ import fs from "fs/promises";
 
 const PATH = "./data/posts.json" // relative to app.js
 
-export async function getPosts() {
+// read all posts from json file
+export async function readPosts() {
     try {
         const data = await fs.readFile(PATH, "utf-8");
         console.log(data);
+
         const json = await JSON.parse(data);
         console.log(json);
+
         return json;
+
     } catch (error) {
         return console.error(error);
     }
