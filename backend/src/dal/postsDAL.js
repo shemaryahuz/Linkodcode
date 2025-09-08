1b42f03f2659ac01a22d398296b893ffd64ad39f
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-09-08 10:57:27 +0300

    add posts data to the server with dal

--- backend/src/dal/postsDAL.js
+++ backend/src/dal/postsDAL.js
@@ -1,1 +1,16 @@
 // data access layer for application posts
+import fs from "fs/promises";
+
+const PATH = "./data/posts.json" // relative to app.js
+
+export async function getPosts() {
+    try {
+        const data = await fs.readFile(PATH, "utf-8");
+        console.log(data);
+        const json = await JSON.parse(data);
+        console.log(json);
+        return json;
+    } catch (error) {
+        return console.error(error);
+    }
+}
