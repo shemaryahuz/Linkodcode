a2085cd360e30785bd44a78f0d41ae84c839a8c5
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-09-09 09:41:20 +0300

    add footer and nav to layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 
 import './App.css'
+import Footer from './components/layout/Footer'
 import Header from './components/layout/Header'
+import Nav from './components/layout/Nav'
 
 import HomePage from './pages/HomePage'
 
@@ -10,7 +12,8 @@ function App() {
     <div className='app'>
       <Header />
       <HomePage />
-
+      <Nav />
+      <Footer />
     </div>
   )
 }
---
+++ frontend/src/components/layout/Nav.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="example@.com">1</a></li>
+        <li><a href="example@.com">2</a></li>
+        <li><a href="example@.com">3</a></li>
+      </ul>
+    </nav>
+  )
+}
