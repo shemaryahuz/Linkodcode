de03ead92713092d0f82c5a04a9eb486635a7ed4
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-09-09 16:05:29 +0300

    add functions to get and add post to posts controller

--- backend/src/routes/postsRouter.js
+++ backend/src/routes/postsRouter.js
@@ -1,9 +1,13 @@
 // router for '/posts' endpoints
 import express from "express";
-import { getAllPosts } from "../controllers/postsController.js";
+import { addPost, getAllPosts, getPost } from "../controllers/postsController.js";
 
 const postsRouter = express.Router();
 
 postsRouter.get("/", getAllPosts);
+
+postsRouter.get("/:id", getPost);
+
+postsRouter.post("/", addPost);
 
 export default postsRouter;
