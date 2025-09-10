eadeeeadfa15e5c159939903c743e06f46c41b6f
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-09-10 11:22:52 +0300

    move middlewares dir to src

--- backend/src/routes/postsRouter.js
+++ backend/src/routes/postsRouter.js
@@ -1,7 +1,7 @@
 // router for '/posts' endpoints
 import express from "express";
 import { addPost, getAllPosts, getPost } from "../controllers/postsController.js";
-import { validatePost } from "../../middlewares/postsMiddlware.js";
+import { validatePost } from "../middlewares/postsMiddlware.js";
 
 const postsRouter = express.Router();
 
